95cfca1f0a19d793813c8dc80150dfdca3307fc3
Olya <ponomaryova.olya.y@gmail.com>
2022-06-02 14:25:56 +0200

    Show the forecast data using API

--- src/app.js
+++ src/app.js
@@ -21,9 +21,9 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function showForecast() {
-  let forecastElement = document.querySelector("#forecast");
-  let forecastHTML = "";
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
   let days = [
     "Sunday",
     "Monday",
@@ -33,34 +33,48 @@ function showForecast() {
     "Friday",
     "Saturday",
   ];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `<div class="row">
-      <div class="col-5">${day}<br />April 28</div>
-        <div class="col-3"><img src="images/partly_cloudy.png"></div>
+
+  return days[day];
+}
+
+function showForecast(response) {
+  console.log(response.data.daily);
+  let forecast = response.data.daily;
+
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = "";
+
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 5) {
+      forecastHTML =
+        forecastHTML +
+        `<div class="row">
+      <div class="col-5">${formatDay(forecastDay.dt)}</div>
+        <div class="col-3"><img src="http://openweathermap.org/img/wn/${
+          forecastDay.weather[0].icon
+        }@2x.png"></div>
         <div class="col-3 column-temp text-end">
-          <span class="extended-forecast-temp-max"> 18° </span>
+          <span class="extended-forecast-temp-max">${Math.round(
+            forecastDay.temp.max
+          )}°</span>
         </div>
                 <div class="col-1 column-temp text-start">
-          <span class="extended-forecast-temp-min"> 9° </span>
+          <span class="extended-forecast-temp-min">${Math.round(
+            forecastDay.temp.min
+          )}°</span>
                 </div>
                 </div>`;
+    }
   });
+
   forecastElement.innerHTML = forecastHTML;
-  console.log(forecastHTML);
 }
 
-function searchTemperature(event) {
-  event.preventDefault();
-  let searchInput = document.querySelector("#search-input");
-  getWeather(searchInput.value);
-}
-
-function getWeather(city) {
+function getForecast(coordinates) {
+  console.log(coordinates);
   let apiKey = "36b22067e3dcdcc365ae1ae08b781c20";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(showTemperature);
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showForecast);
 }
 
 function showTemperature(response) {
@@ -90,11 +104,16 @@ function showTemperature(response) {
   getForecast(response.data.coord);
 }
 
-function getForecast(coordinates) {
-  console.log(coordinates);
+function searchCity(city) {
   let apiKey = "36b22067e3dcdcc365ae1ae08b781c20";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(showForecast);
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-input");
+  searchCity(searchInput.value);
 }
 
 function showFahrenheitTemp(event) {
@@ -118,13 +137,14 @@ function showCelsiusTemp(event) {
 }
 
 let celsiusTemperature = null;
-showForecast();
 
 let searchButton = document.querySelector("#submit-button");
-searchButton.addEventListener("click", searchTemperature);
+searchButton.addEventListener("click", handleSubmit);
 
 let fahrenheitButton = document.querySelector("#fahrenheitButton");
 fahrenheitButton.addEventListener("click", showFahrenheitTemp);
 
 let celsiusButton = document.querySelector("#celsiusButton");
 celsiusButton.addEventListener("click", showCelsiusTemp);
+
+searchCity("Kyiv");
